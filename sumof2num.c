43a284123b572bbe08c7fb298732ca4e8667f27c
OjasCodes <ojascancode@gmail.com>
2024-10-30 13:10:11 +0530

    Q2-HW1

---
+++ sumof2num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a , b;
+    printf("a:");
+    scanf("%d" , &a);
+    printf("b:");
+    scanf("%d" , &b);
+    int sum = a + b ;
+    printf("sum of two numbers: %d" , sum );
+    return 0;
+}
