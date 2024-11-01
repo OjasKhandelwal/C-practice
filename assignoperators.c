d9b410d5f2e5670db4d5e44ddcfeabab02545cf8
OjasCodes <ojascancode@gmail.com>
2024-11-01 13:13:42 +0530

    HW2-Q3

---
+++ assignoperators.c
@@ -0,0 +1,23 @@
+//Assignment Operators
+#include <stdio.h>
+
+int main() {
+    int x , y;
+    
+    printf("x:");
+    scanf("%d" , &x);
+    printf("y:");
+    scanf("%d" , &y);
+    
+    printf("x+=y: %d \n" , x+=y);
+    printf("x-=y: %d \n" , x-=y);
+    printf("x*=y: %d \n" , x*=y);
+    printf("x/=y: %d \n" , x/=y);
+    printf("x%=y: %d \n" , x%=y);
+    printf("x<<=y: %d \n" , x<<=y);
+    printf("x^=y: %d \n" , x^=y);
+    printf("x>>=y: %d \n" , x>>=y);
+    printf("x&=y: %d " , x&=y);
+    
+    return 0;
+}
