b9379c7f81d87bbecb4228304bd04621fc63a4d0
OjasCodes <ojascancode@gmail.com>
2024-11-01 13:40:53 +0530

    HW2-Q5

---
+++ incdicoperator.c
@@ -0,0 +1,16 @@
+//Increment decrement operators
+#include <stdio.h>
+
+int main() {
+    int x ;
+    
+    printf("x:");
+    scanf("%d" , &x);
+    
+    printf("++x: %d \n" , ++x);
+    printf("--x: %d \n" , --x);
+    printf("x++: %d \n" , x++);
+    printf("x--: %d \n" , x--);
+    
+    return 0;
+}
