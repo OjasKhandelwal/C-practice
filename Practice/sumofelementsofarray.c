e498a7716a3b9e4b9eeb6f326bc43ccd1bed2984
Ojas Khandelwal <ojascancode@gmail.com>
2024-12-03 20:53:57 +0530

    Update sumofelementsofarray.c

--- Practice/sumofelementsofarray.c
+++ Practice/sumofelementsofarray.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {43 , 6 , 5 , 87 , 69};
-    int sum = arr[0]+arr[1]+arr[2]+arr[3]+arr[4];
+    int sum;
+    int arr[5] = {43 , 6 , 4 , 47 , 69};
+    for(int i = 0 ; i<5 ; i++){
+        sum+=arr[i];
+    }
     printf("sum of elements of array: %d" , sum);
     return 0;
 }
